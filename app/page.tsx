617282a735f7df8e6978538786381b3df82c0a7a
Leon <doncorleone142857@gmail.com>
2024-01-21 09:24:13 +0700

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
 import Image from 'next/image'
-import { ButtonDestructive, ButtonPrimaryLarge, ButtonPrimarySmall, ButtonSecondaryDark, ButtonSecondaryLight } from './component'
+import { ButtonDestructive, ButtonPrimaryLarge, ButtonPrimarySmall, ButtonSecondaryDark, ButtonSecondaryLight, SubtaskCheckbox } from './component'
 
 export default function Home() {
   return (
-    <main className="flex ">
-      <div>
-        <ButtonPrimaryLarge text={'hello there'} ></ButtonPrimaryLarge>
-      </div>
-      <div>
-        <ButtonPrimarySmall text={'hello there'} ></ButtonPrimarySmall>
-      </div>
-      <div className='bg-[white] w-fit'>
-        <ButtonSecondaryLight text={'hello there'} ></ButtonSecondaryLight>
-      </div>
-      <div className='bg-[black]'>
-        <ButtonSecondaryDark text={'hello there'} ></ButtonSecondaryDark>
-      </div>
-      <ButtonDestructive text={'hello there'} ></ButtonDestructive>
+    <main className="flex flex-col gap-y-8">
+      <section className='flex flex-col'>
+        <h1>Buttons</h1>
+        <section className='flex flex-wrap gap-3 pt-3'>
+          <div>
+            <ButtonPrimaryLarge text={'Button Primary Large'} ></ButtonPrimaryLarge>
+          </div>
+          <div>
+            <ButtonPrimarySmall text={'Button Primary Small'} ></ButtonPrimarySmall>
+          </div>
+          <div className='bg-[white] w-fit'>
+            <ButtonSecondaryLight text={'Button Secondary Light'} ></ButtonSecondaryLight>
+          </div>
+          <div className='bg-[black]'>
+            <ButtonSecondaryDark text={'Button Secondary Dark'} ></ButtonSecondaryDark>
+          </div>
+          <ButtonDestructive text={'Button Destructive'} ></ButtonDestructive>
+        </section>
+      </section>
+      <section className='flex flex-col'>
+        <h1 className='mb-4'>Subtask Checkbox</h1>
+        <SubtaskCheckbox></SubtaskCheckbox>
+      
+      </section>
       {/* <ButtonPrimaryLarge text={'hello there'} ></ButtonPrimaryLarge> */}
       {/* <ButtonPrimaryLarge text={'hello there'} ></ButtonPrimaryLarge> */}
     </main>
