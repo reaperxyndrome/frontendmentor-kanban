02214b2df5004af8fd1ad5ed81ee190bdd55503c
Leon <doncorleone142857@gmail.com>
2024-02-05 10:58:17 +0700

    created a test suite for text field

--- app/component/TextField.test.tsx
+++ app/component/TextField.test.tsx
@@ -3,11 +3,58 @@
  */
 
 // import '@testing-library/jest-dom'
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import { TextField } from '@/app/component/TextField';
 
-it("should have hello label", () => {
-  render(<TextField placeholder={''} required={false} isSubmitted={false} id={''} label={'hello'} />)
-  const element = screen.getByText("hello")
-  expect(element).toBeInTheDocument()
+describe("TextField", () => {
+  it("should have label visible", () => {
+    render(<TextField placeholder={'hey there'} required={false} isSubmitted={false} id={'hey'} label={'hello'} />)   
+    const label = screen.getByTestId("label")
+    expect(label).toBeVisible()
+  })
+  
+  it("should have input and placeholder visible", () => {
+    render(<TextField placeholder={'hey there'} required={false} isSubmitted={false} id={'hey'} label={'hello'} />)  
+    const input = screen.getByTestId("input")
+    const placeholder = screen.getByPlaceholderText("hey there")
+    expect(input).toHaveAttribute("placeholder", "hey there")
+    expect(placeholder).toBeVisible()
+  })
+
+  it("should be able to receive keyboard input", () => {
+    render(<TextField required={true} isSubmitted={true} id={'hey'} label={'hello'} />)  
+    const inputElement = screen.getByPlaceholderText("Add task") as HTMLInputElement
+    fireEvent.change(inputElement, {target: {value: "input changed"}})
+    expect(inputElement.value).toBe("input changed")
+  })
+  it("should have warning message when input is empty and isSubmitted and required", () => {
+    render(<TextField required={true} isSubmitted={true} id={'hey'} label={'hello'} />)  
+    const warningMessage = screen.getByText("Can't be empty")
+    expect(warningMessage).toBeVisible()
+  })
+  it("should not have warning message when input is not empty and isSubmitted and required", () => {
+    render(<TextField required={true} isSubmitted={true} id={'hey'} label={'hello'} />)  
+    const inputElement = screen.getByPlaceholderText("Add task") as HTMLInputElement
+    fireEvent.change(inputElement, {target: {value: "input changed"}})
+    const warningMessage = screen.queryByText("Can't be empty")
+    expect(warningMessage).toBeNull()
+  })
+  it("should not have warning message when input is empty and isSubmitted and not required", () => {
+    render(<TextField required={false} isSubmitted={true} id={''} label={''}></TextField>)
+    const warningMessage = screen.queryByText("Can't be empty")
+    expect(warningMessage).toBeNull() 
+  })
+  it("should not have warning message when input is not empty and isSubmitted and not required", () => {
+    render(<TextField required={false} isSubmitted={true} id={''} label={''}></TextField>)
+    const inputElement = screen.getByPlaceholderText("Add task") as HTMLInputElement
+    fireEvent.change(inputElement, {target: {value: "input changed"}})
+    const warningMessage = screen.queryByText("Can't be empty")
+    expect(warningMessage).toBeNull()
+  })
+  it("should be able to be focused", () => {
+    render(<TextField required={false} isSubmitted={false} id={''} label={''}></TextField>)
+    const inputElement = screen.getByPlaceholderText("Add task") as HTMLInputElement
+    inputElement.focus()
+    expect(inputElement).toHaveFocus()
+  })
 })
